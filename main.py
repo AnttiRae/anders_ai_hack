d17a4d940427148f3b9a148979da0fa7b90d97b8
Antti Rae <anttirae@gmail.com>
2024-05-10 19:16:56 +0300

    typos

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ except Exception as e:
 
 model = GPT4All("orca-mini-3b-gguf2-q4_0.gguf", device=GPU)
 
-with model.chat_session(system_prompt="You are a player of text adventure game. You must choose your actions wisely and make beneficial choices. Dont mention anything about being an AI or being unable to to physical things. You must end your answer always with a decision for the player. The player with not make any decisions."):
+with model.chat_session(system_prompt="You are a player of text adventure game. You must choose your actions wisely and make beneficial choices. Dont mention anything about being an AI or being unable to do physical things. You must end your answer always with a decision for the player. The player will not make any decisions"):
     response = model.generate(prompt="You are presented with a set doors. On the left you see a door with a skull and bones symbol. On your right you see a door with a symbol of food. What do you do?", temp=0)
     print(model.current_chat_session[2])
 
