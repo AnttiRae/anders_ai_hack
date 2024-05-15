c6e605a83c1577b87c9baa28db8454c1eb70b1ca
Antti Rae <anttirae@gmail.com>
2024-05-15 21:58:10 +0300

    2 models trying to communicate, but got core dumped error instead for now...

--- main.py
+++ main.py
@@ -1,14 +1,33 @@
-from gpt4all import GPT4All, gpt4all
+from gpt4all import GPT4All
 
-try:
-    GPU = GPT4All.list_gpus()[0]
-except Exception as e:
-    print("no GPU found lol", e)
-    quit()
+from prompts import GAME_SETUP_MESSAGE, GAMEMASTER_SYSTEM_MESSAGE, PLAYER_SYSTEM_MESSAGE
 
-model = GPT4All("orca-mini-3b-gguf2-q4_0.gguf", device=GPU)
+# kiva 
+LLM_MODEL = "orca-mini-3b-gguf2-q4_0.gguf"
 
-with model.chat_session(system_prompt="You are a player of text adventure game. You must choose your actions wisely and make beneficial choices. Dont mention anything about being an AI or being unable to do physical things. You must end your answer always with a decision for the player. The player will not make any decisions"):
-    response = model.generate(prompt="You are presented with a set doors. On the left you see a door with a skull and bones symbol. On your right you see a door with a symbol of food. What do you do?", temp=0)
-    print(model.current_chat_session[2])
+def print_gamestate(master_state, player_state):
+    print(f"{master_state.current_chat_session} {player_state.current_chat_session[-1]}")
+
+def main():
+    try:
+        GPU = GPT4All.list_gpus()[0]
+    except Exception as e:
+        print("no GPU found lol", e)
+        quit()
+
+    player = GPT4All(LLM_MODEL, device=GPU)
+    game_master = GPT4All(LLM_MODEL, device=GPU)
+    print("Starting...")
+    with game_master.chat_session(system_prompt=GAMEMASTER_SYSTEM_MESSAGE):
+        # lets start with temp 0, so we get always the same start for our epic adventure.
+        game_master.generate(prompt=GAME_SETUP_MESSAGE, temp=0)  
+        with player.chat_session(system_prompt=PLAYER_SYSTEM_MESSAGE):
+            for x in range(3):
+                player.generate(prompt=game_master.current_chat_session[-1])
+                game_master.generate(prompt=player.current_chat_session[-1])
+                print_gamestate(master_state=game_master.current_chat_session, player_state=player.current_chat_session)
+
+
+if __name__ == "__main__":
+    main()
 
