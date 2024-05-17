e340172bc385a3cb0e52d9629ff4c8c1853aef05
Antti Rae <anttirae@gmail.com>
2024-05-17 23:01:28 +0300

    basic logic for gameloop

--- main.py
+++ main.py
@@ -1,31 +1,65 @@
 from gpt4all import GPT4All
+from paramiko.client import SSHClient
+import select
+from prompts import PLAYER_SYSTEM_MESSAGE
+import time
 
-from prompts import GAME_SETUP_MESSAGE, GAMEMASTER_SYSTEM_MESSAGE, PLAYER_SYSTEM_MESSAGE
-
-# kiva 
 LLM_MODEL = "orca-mini-3b-gguf2-q4_0.gguf"
+
 
 def print_gamestate(master_state, player_state):
     print(f"{master_state.current_chat_session} {player_state.current_chat_session[-1]}")
 
+
 def main():
+
+    # setup gpu and model stuff
     try:
         GPU = GPT4All.list_gpus()[0]
-    except Exception as e:
-        print("no GPU found lol", e)
-        quit()
+    except Exception:
+        print("No GPU found, running with no GPU :(")
+        GPU = None
 
-    player = GPT4All(LLM_MODEL, device=GPU)
-    game_master = GPT4All(LLM_MODEL, device=GPU)
-    print("Starting...")
-    with game_master.chat_session(system_prompt=GAMEMASTER_SYSTEM_MESSAGE):
-        # lets start with temp 0, so we get always the same start for our epic adventure.
-        game_master.generate(prompt=GAME_SETUP_MESSAGE, temp=0)  
-        with player.chat_session(system_prompt=PLAYER_SYSTEM_MESSAGE):
-            for x in range(3):
-                player.generate(prompt=game_master.current_chat_session[-1])
-                game_master.generate(prompt=player.current_chat_session[-1])
-                print_gamestate(master_state=game_master.current_chat_session, player_state=player.current_chat_session)
+    if GPU is None:
+        llm_player = GPT4All(LLM_MODEL)
+    else:
+        llm_player = GPT4All(LLM_MODEL, device=GPU)
+
+    ssh_client = SSHClient()
+    ssh_client.load_system_host_keys()
+    ssh_client.connect("172.17.0.2", username='bilbo', password='bilbo')
+    #  stdin, stdout, stderr = ssh_client.exec_command('cd BashVenture; ./adventure.sh')
+    transport = ssh_client.get_transport()
+    channel = transport.open_session()
+    channel.get_pty()
+    channel.exec_command('cd BashVenture; ./adventure.sh')
+    time.sleep(5)
+
+    vitsi_muuttuja_lol = ''
+
+    while True:
+        # readable list, writable list, exception list. Maybe I'll use them...
+        rl, wl, xl = select.select([channel], [], [], 4)
+        game_output = ''
+        if len(rl) > 0:
+            game_output = channel.recv(1024).decode()
+            vitsi_muuttuja_lol += game_output
+            print(game_output)
+
+        else:  # game seems to be waiting for user input!
+            print('lollero', vitsi_muuttuja_lol)
+            if '[ENTER]' in vitsi_muuttuja_lol:
+                print('commence pressing of the enter')
+                channel.send(b'\n')
+                print('entered enter')
+                vitsi_muuttuja_lol = ''
+            elif '>' in vitsi_muuttuja_lol:
+                print('oh no we must ask LLM FOR HELP!!!!')
+                llm_input = vitsi_muuttuja_lol.split("--- Classic Adventure for Bash ---")[-1].strip()
+                print('lets ask this from our AI friend:', llm_input)
+                with llm_player.chat_session(system_prompt=PLAYER_SYSTEM_MESSAGE):
+                    llm_player.generate(prompt=llm_input)
+                    print('our AI friend said this:', llm_player.current_chat_session[-1])
 
 
 if __name__ == "__main__":
--- prompts.py
+++ prompts.py
@@ -1,9 +1,3 @@
 PLAYER_SYSTEM_MESSAGE = """
-You are a player of text adventure game. You must choose your actions wisely and make beneficial choices. Dont mention anything about being an AI or being unable to do physical things. You must end your answer always with a decision for the player. The player will not make any decisions
-"""
-GAMEMASTER_SYSTEM_MESSAGE = """
-You are a gamemaster. Your task is to generate a text based adventure for the player of this game. Provide the player with few choices of action and describe what is going on in your game. Keep the messages short.
-"""
-GAME_SETUP_MESSAGE = """
-Your game theme should be a high fantasy adventure with all kinds of different races like elfs, dwarfes and gnomes.
+You are a player of text adventure game. You must choose your actions wisely and make beneficial choices. Dont mention anything about being an AI or being unable to do physical things. You must end your answer always with a decision for the player. The player will not make any decisions. You have the following choices to respond with: 'n' for north, 's' for south, 'e' for east, 'w' for west, 'u' for use, 'h' for hug.
 """
